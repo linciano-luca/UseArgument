86e5c1ebc278de4d873ceb15a6458b73ba2ad9e4
Luca Linciano <luca.linciano@hotmail.it>
2018-12-11 10:28:29 +0100

    First commit

---
+++ src/useargument/UseArgument.java
@@ -0,0 +1,15 @@
+
+package useargument;
+
+
+public class UseArgument {
+
+    
+    public static void main(String[] args) {
+        System.out.print("Hi, ");
+        System.out.print(args[0]);
+        System.out.print(". How are you?");
+        System.out.println();
+    }
+    
+}
